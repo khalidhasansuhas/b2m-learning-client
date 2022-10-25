cc3ce949455d728ee3306bf6a9fe50cbff4afcda
khalidhasansuhas <khalidhasansuhas@gmail.com>
2022-10-25 22:19:25 +0600

    left side nav added

--- src/Pages/Shared/LeftSideNav/LeftSideNav.js
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.js
@@ -1,35 +1,31 @@
 import React from 'react';
-import Carousel from 'react-bootstrap/Carousel';
-import Shanto from '../../../assets/Shanto.jpg';
-import Mustafiz from '../../../assets/Mustafiz.jpg';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import { Link } from 'react-router-dom';
+import Announcement from '../../../assets/Announcement.jpg';
+
 
 const LeftSideNav = () => {
     return (
-        <Carousel>
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src={Shanto}
-                    alt="First slide"
-                />
-                <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a .</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src={Mustafiz}
-                    alt="Second slide"
-                />
-
-                <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-        </Carousel>
+        <div >
+            <Card className='shadow p-3 mb-5 bg-body rounded' style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={Announcement} />
+                <Card.Body>
+                    <Card.Title>Discount Offer!!!</Card.Title>
+                    <Card.Text>
+                        20% off for upcoming enrollment for any courses for everyone who purchase our course before 11/11/22.
+                    </Card.Text>
+                </Card.Body>
+                <ListGroup className="list-group-flush">
+                    <ListGroup.Item>Previous Value: 10000</ListGroup.Item>
+                    <ListGroup.Item>Discounted Value: 8000</ListGroup.Item>
+                    <ListGroup.Item>Save: 2000</ListGroup.Item>
+                </ListGroup>
+                <Card.Body className='text-center'>
+                    <Link to='courses'><button type="button" class="btn btn-primary">Explore</button></Link>
+                </Card.Body>
+            </Card>
+        </div>
     );
 };
 
